667e50f9c2314e2d1592abbba913e6b4562e57b5
anthonyedm <anthony@emddigital.com.au>
2025-08-11 09:35:37 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,25 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch(`https://api.employmenthero.com/careers-page/jobs?org_id=ee9fb5ab-9d97-4839-93d6-12e8259a46ee`, {
-      headers: {
-        "Authorization": "Bearer V_IWxAEnViT3Vku_CSYL7A",
-        "Content-Type": "application/json"
+    const response = await fetch(
+      "https://api.employmenthero.com/public/v1/careers/jobs",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "org-id": "ee9fb5ab-9d97-4839-93d6-12e8259a46ee",
+          "Authorization": "Bearer V_IWxAEnViT3Vku_CSYL7A"
+        }
       }
-    });
+    );
 
     if (!response.ok) {
-      return res.status(response.status).json({ error: `Employment Hero API error: ${response.statusText}` });
+      return res.status(response.status).json({ error: "Employment Hero API error" });
     }
 
     const data = await response.json();
-    return res.status(200).json(data);
-    
+    res.status(200).json(data);
+
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Failed to load jobs" });
+    res.status(500).json({ error: error.message });
   }
 }
